ff76c46e2ed456ebe8be01f0aa6a0ec0eca76c13
Azkaaysar <pribadiazka4@gmail.com>
2023-10-17 22:55:39 +0700

    bismillah

--- index.php
+++ index.php
@@ -1,41 +1,68 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Encrypt</title>
 
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/bootstap-theme.css">
-  <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<head>
+    <title>Encrypt</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+    <!-- Custom CSS -->
+    <style>
+        body {
+            background-color: #f5f5f5;
+            font-family: 'Arial', sans-serif;
+        }
+
+        .container {
+            margin-top: 100px;
+            max-width: 400px;
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .btn-default {
+            background-color: #2c3e50;
+            color: #ecf0f1;
+            border: none;
+        }
+
+        .btn-default:hover,
+        .btn-default:focus {
+            background-color: #34495e;
+            color: #ecf0f1;
+        }
+    </style>
 </head>
+
 <body>
 
-<div class="container">
-  <div class='row'>
-      <div class='col-sm-12'>
-      <!-- Bootstrap form-->
-    <form action="login.php" method="POST" role='form'>
-      <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" name="username" placeholder="Enter your username" >
-          </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" name="password" placeholder="Password">
-      </div>
+    <div class="container">
+        <div class='row'>
+            <div class='col-sm-12'>
+                <!-- Bootstrap form-->
+                <form action="login.php" method="POST" role='form'>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" name="username" placeholder="Enter your username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
 
-      <!--Button-->
-          <input type="submit" class="btn btn-default" value="Sign In">.
-    </form>
-      <br><br>  
-      <text class="text-muted">Not registered? Sign up easily!</textarea>
-      <br>
-      <!--Button pseudoclass according to bootstrap-->
-      <a href="new_user.php" class="btn btn-default" role="button">Sign Up </a>
-
-  </div>
-
-  </div>
-</div>
+                    <!--Button-->
+                    <input type="submit" class="btn btn-default" value="Sign In">
+                </form>
+                <br>
+                <p class="text-muted">Not registered? Sign up easily!</p>
+                <br>
+                <!--Button pseudoclass according to bootstrap-->
+                <a href="new_user.php" class="btn btn-default" role="button">Sign Up</a>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
