0ccd46e902d52bfa43e25f7ba009cb585c7d534c
teukuauli <teukuauli18@gmail.com>
2023-10-17 23:23:17 +0700

    database

---
+++ db/users.php
@@ -0,0 +1,50 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Create the database
+$sql = "CREATE DATABASE users";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+// Create the tables
+$sql = "CREATE TABLE users.personal_data (
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table personal_data created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+$sql = "CREATE TABLE users.private_data (
+  id_card_image BLOB NOT NULL,
+  pdf_file BLOB NOT NULL,
+  doc_file BLOB NOT NULL,
+  xls_file BLOB NOT NULL,
+  video_file BLOB NOT NULL,
+  aes_encrypted_data BLOB NOT NULL,
+  rc4_encrypted_data BLOB NOT NULL,
+  de_encrypted_data BLOB NOT NULL
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table private_data created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+//mysqli_real_escape_string()  escapes special characters in a string for use in an SQL statement
+$id_card_image = mysqli_real_escape_string($conn, $id_card_image);
+$pdf_file = mysqli_real_escape_string($conn, $pdf_file);
+$doc_file = mysqli_real_escape_string($conn, $doc_file);
+$xls_file = mysqli_real_escape_string($conn, $doc_file);
---
+++ db/users.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 07, 2017 at 08:47 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `practise`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  personal_data VARCHAR(255) NOT NULL,
+  private_data VARCHAR(255) NOT NULL,
+  id_card_image BLOB NOT NULL,
+  pdf_file BLOB NOT NULL,
+  doc_file BLOB NOT NULL,
+  xls_file BLOB NOT NULL,
+  video_file BLOB NOT NULL
+);
