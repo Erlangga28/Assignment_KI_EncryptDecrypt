718527b38f776f0896b8a3033cfd566bff74abdf
rahadianpra <rahadianprayitno13@gmail.com>
2023-10-17 19:46:30 +0700

    databases new

--- login.php
+++ login.php
@@ -1,12 +1,40 @@
 <?php
-$host = "localhost"; // Ganti dengan host database Anda
-$username = "root"; // Ganti dengan username database Anda
-$password = ""; // Ganti dengan password database Anda
-$database = "projectusm"; // Ganti dengan nama database Anda
+	//For connecting the database
+	$link = mysqli_connect("localhost","root","", "practise");
+	if (!$link) {
+	    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	    exit;
+	}
+	
 
-$conn = mysqli_connect("localhost", "root", "", "projectusm");
+session_start();
+	$usern = $_POST['username']; //using post method's username to set variable
+	$_SESSION["username"]=$usern; //declaring session variable and putting usern declared above
+	echo $_SESSION["username"];
 
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
+	$input_password = $_POST['password'];
+	$salt = ('nepal');
+	$hashed_password= crypt($input_password, $salt); //no password decryption for crypt() function
+
+	//fetch previous password
+		$query = mysqli_query($link, "SELECT password FROM users WHERE username='$usern' ")or die(mysql_error());
+		$row = mysqli_fetch_row($query);
+		$previous_password = $row[0];
+			 
+
+	
+	if ($hashed_password==$previous_password) {
+		header("Location: user_dashboard.php");
+		die();
+	}else{
+		echo "Wrong username and/or password";
+	}
+
+		
+	mysqli_close($link);
+				
 ?>
+
+		
