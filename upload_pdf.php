bc1a9168a71510bad90581d17de265458fd44c7a
Erlangga Wahyu Utomo <erlanggawahyu21@gmail.com>
2023-10-18 00:21:31 +0700

    add image

--- upload_pdf.php
+++ upload_pdf.php
@@ -11,7 +11,7 @@
     <div class="container">
     <div class="row">
     <div class="col-md-8">
-    <h1><a href="#" target="_blank"><img src="logo.png" width="80px"/>File Uploading</a></h1>
+    <h1><a href="#" target="_blank"><img src="image/upload.png" width="80px"/>File Uploading</a></h1>
     <hr> 
     <form id="form" action="ajaxupload.php" method="post" enctype="multipart/form-data">
     <div class="form-group">
@@ -23,7 +23,7 @@
     <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" required />
     </div>
     <input id="uploadImage" type="file" accept="image/*" name="image" />
-    <div id="preview"><img src="filed.png" /></div><br>
+    <div id="preview"><img src="image/preview.png" /></div><br>
     <input class="btn btn-success" type="submit" value="Upload">
     </form>
     <div id="err"></div>
